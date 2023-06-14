1b5c94c18286473ca4396c200fb2bb412318980b
Philipp Bartels | Handmade Systems <pb@handmade-systems>
2023-06-14 22:52:50 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: [
+        require('prettier-plugin-packagejson'),
+        require('@trivago/prettier-plugin-sort-imports'),
+        require('prettier-plugin-tailwindcss'),
+    ],
+    trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    importOrder: ['<THIRD_PARTY_MODULES>','^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    endOfLine: 'lf',
+}
